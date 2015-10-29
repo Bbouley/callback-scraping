61280b39479a2fb3538b901ce114e93c3d19b0ed
bbouley <b.bouley@hotmail.co.uk>
2015-10-29 16:10:10 -0600

    working on refactor using helper functions

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,52 +3,43 @@ var router = express.Router();
 var request = require('request');
 var cheerio = require('cheerio');
 
+    var urls = {
+        hackerUrl : 'https://news.ycombinator.com/',
+        redditUrl : 'https://www.reddit.com/r/Web_Development/',
+        pythonUrl : 'https://www.python.org/',
+        MDNUrl : 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+    };
+
+
+    function hasJavascript(url, selector, cb) {
+
+        return request(url, function(err, response, html) {
+            var $ = cheerio.load(html);
+            var title = $(selector).first().text();
+            console.log(title);
+
+                if(title.match('javascript')) {
+                    cb(null, true);
+                } else {
+                    cb(null, false);
+                }
+        });
+    }
+
+
+
 router.get('/', function(req, res, next) {
-
-    var mdnText = '';
-    var pyText = '';
-
-  request.get('https://news.ycombinator.com/', function(err, response, body) {
-    if(err) {
-        res.send(err);
-    } else {
-        var $ = cheerio.load(body);
-        var titles = ($('.title')).text();
-        var firstTitle = titles.split('2.')[0];
-        if(firstTitle.indexOf('Javascript')>=0 || firstTitle.indexOf('javaScript') >=0) {
-            request.get('https://developer.mozilla.org/en-US/docs/Web/JavaScript', function(err2, res2, body2) {
-                $ = cheerio.load(body2);
-                mdnText = ($('body')).html();
-                res.send({'Here is mdn. Use it wisely...' : mdnText});
-            });
+    var test = (hasJavascript(urls.hackerUrl, 'td.title a', function(err, results) {
+        if(err) {
+            console.log('Something went wrong');
         } else {
-         request.get('https://www.reddit.com/r/Web_Development/', function(err, response, body) {
-            if(err) {
-                res.send(err);
-            } else {
-                var $ = cheerio.load(body);
-                var titles = $('.thing').text();
-                var firstTitle = titles.split('submitted')[0];
-
-                     if(firstTitle.indexOf('javascript')>=0 || firstTitle.indexOf('javaScript') >=0) {
-                        request.get('https://developer.mozilla.org/en-US/docs/Web/JavaScript', function(err2, res2, body2) {
-                        $ = cheerio.load(body2);
-                        mdnText = ($('.title')).text();
-                        res.send({'Here is mdn. Use it wisely...': mdnText});
-                        });
-
-                    } else {
-                        request.get('https://www.python.org/', function(err3, res3, body3) {
-                        $ = cheerio.load(body3);
-                        pyText = ($('title')).text();
-                        res.send({'Pythons totally cool... ': pyText});
-                        });
-                       }
-                    }
-               });
-           }
+            return results;
         }
-    });
+    }));
+
+    console.log(test);
+
+
 });
 
 
