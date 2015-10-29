5b25c683319d8d78e9909b49149c020418432540
bbouley <b.bouley@hotmail.co.uk>
2015-10-29 16:40:00 -0600

    added in get router

--- server/routes/index.js
+++ server/routes/index.js
@@ -16,7 +16,6 @@ var cheerio = require('cheerio');
         return request(url, function(err, response, html) {
             var $ = cheerio.load(html);
             var title = $(selector).first().text();
-            console.log(title);
 
                 if(title.match('javascript')) {
                     cb(null, true);
@@ -26,19 +25,39 @@ var cheerio = require('cheerio');
         });
     }
 
+    function getFun(url, selector, cb) {
+
+        request(url, function(err, response, html) {
+            var $ = cheerio.load(html);
+            var fun = $(selector);
+            cb(null, fun);
+        });
+
+    }
+
 
 
 router.get('/', function(req, res, next) {
-    var test = (hasJavascript(urls.hackerUrl, 'td.title a', function(err, results) {
-        if(err) {
-            console.log('Something went wrong');
+
+    var test = hasJavascript(urls.hackerUrl, 'td.title a', function(err, results) {
+        if(!results) {
+            getFun(urls.pythonUrl, 'img', function(err, results) {
+                res.send(results.attr('src'));
+            });
         } else {
-            return results;
+            hasJavascript(urls.redditUrl, 'a.title', function(err ,results) {
+                if(!results) {
+                    getFun(urls.pythonUrl, 'img', function(err, results) {
+                        res.send(results.attr('src'));
+                    });
+                } else {
+                    getFun(urls.mozilla, 'img', function(err, results) {
+                        res.send(results.attr('src'));
+                    });
+                }
+            });
         }
-    }));
-
-    console.log(test);
-
+    });
 
 });
 
