241b471fd89963a05c8386df0a64e6353724eda8
bbouley <b.bouley@hotmail.co.uk>
2015-10-30 14:15:33 -0600

    added chained promise to hit two sites in a row, moved original commented code to bottom

--- server/routes/logic.js
+++ server/routes/logic.js
@@ -1,5 +1,83 @@
 var ourResult = true;
 var request = require('request');
+var cheerio = require('cheerio');
+
+
+function scraper(url, selector, cb) {
+    request.get(url, function(err, res, html) {
+        if(res && res.statusCode === 200) {
+            $ = cheerio.load(html);
+            var titles = $(selector);
+            var output = [];
+            [].forEach.call(titles, function(el) {
+                output.push($(el).text());
+            });
+            cb(null, output[0]);
+        } else {
+            cb(err);
+        }
+    });
+}
+
+var hackerNewsPromise = new Promise(function(resolve, reject) {
+    scraper('https://news.ycombinator.com/', 'td.title a', function(err, result) {
+        if(err) {
+            reject (err);
+        } else {
+            resolve (result);
+        }
+    });
+});
+
+var redditPromise = new Promise(function(resolve, reject) {
+    scraper('https://www.reddit.com/r/Web_Development/', 'a.title', function(err, result) {
+        if(err) {
+            reject(err);
+        } else {
+            resolve (result);
+        }
+    });
+});
+
+var keyword = 'javascript';
+
+
+hackerNewsPromise.then(function(result) {
+    if(result.match(keyword)) {
+        console.log('--------------------------------');
+        console.log(result);
+        console.log('Hacker News Has Javascript');
+        return redditPromise;
+    } else {
+        console.log('--------------------------------');
+        console.log(result);
+        console.log('NO JAVASCRIPT FROM HACKER NEWS');
+        return redditPromise;
+    }
+}).catch(function(err) {
+        console.log('--------------------------------');
+        console.log(error);
+        console.log('ERROR ERROR ERROR ERROR');
+}).then(function(result) {
+    if(result.match(keyword)) {
+        console.log('-------------------------------');
+        console.log(result);
+        console.log('Reddit Has Javascript');
+    } else {
+        console.log('--------------------------------');
+        console.log(result);
+        console.log('NO JAVASCRIPT FROM REDDIT');
+    }
+}).catch(function(err) {
+        console.log('--------------------------------');
+        console.log(error);
+        console.log('ERROR ERROR ERROR ERROR');
+});
+
+
+
+//*** This Code was for simple promises ***///
+
 
 var promise = new Promise(function (resolve, reject) {
     if(ourResult) {
@@ -10,64 +88,38 @@ var promise = new Promise(function (resolve, reject) {
         }
 });
 
-// promise.then(function(result) {
-//     console.log('Result: ', result);
-// }).catch(function(result) {
-//     console.log('Reject: ',result);
-// });
-
 var successFn = function(message) {
-    console.log('Success: ', message);
+    // console.log('Success: ', message);
     return message;
 };
 
 var errorFn = function(message) {
-    console.log('Error:', message);
+    // console.log('Error:', message);
     return message;
 };
 
-// promise.then(successFn, errorFn).then(function(result) {
-//     console.log(result + '!!!!!!!!!!!!!');
-// });
+promise.then(successFn, errorFn).then(function(result) {
+    // console.log(result + '!!!!!!!!!!!!!');
+    // console.log('-------------------------');
+});
 
 promise.then(function(result) {
     return 10;
 }).then(function(result) {
-    console.log('Result 1 : ', result);
+    // console.log('Result 1 : ', result);
     return result * 10;
 }).then(function(result) {
-    console.log('Result 2 : ', result);
+    // console.log('Result 2 : ', result);
     return result * 10;
 }).then(function(result) {
-    console.log('Result 3 : ', result);
+    // console.log('Result 3 : ', result);
     throw 'err';
 }).then(function(result) {
-    console.log('Result 4 : ', result);
+    // console.log('Result 4 : ', result);
     return result * 10;
 }).then(function(result) {
-    console.log('Result 5 : ', result);
+    // console.log('Result 5 : ', result);
     return result * 10;
 }).catch(function(err) {
-    console.log(err);
-});
-
-
-var promise2 = new Promise(function(resolve, reject) {
-    request('https://news.ycombinator.com/', function(err, res, html) {
-        if(res.statusCode === 200) {
-            resolve('Status is : ' + res.statusCode + ' This is good');
-        } else {
-            reject ('Status is : ' + res.statusCode + ' This is bad. very very bad.');
-        }
-    });
-});
-
-promise2.then(function(result, err) {
-    if(result) {
-        console.log('---------------------------------');
-        console.log(result);
-    } else {
-        console.log('---------------------------------');
-        console.log(err);
-    }
+    // console.log(err);
 });
